84a56aa15f7c32a76d60b8ff85bbc65e249cc8ee
Dagi <dagiheywot@gmail.com>
2025-06-25 13:23:28 +0300

    added projects

--- src/data/webProjects.js
+++ src/data/webProjects.js
@@ -1,39 +1,23 @@
 import img from "../../assets/images/dh-l.jpg"
+import taskwaveImg from "../assets/images/screenshots/taskwave.png"
+import portfolio from "../assets/images/screenshots/portfolio.png"
 const projects = [
     {
       "id": 1,
-      "title": "PixelCraft",
-      "description": "A simple drawing app built with HTML5 Canvas and JavaScript. Supports color picking, brush sizes, and save to PNG.",
-      "techStack": ["HTML", "CSS", "JavaScript"],
-      "image": img,
-      "demoLink": "https://your-demo-link.com/pixelcraft",
-      "codeLink": "https://github.com/yourusername/pixelcraft"
+      "title": "TaskWave",
+      "description": "A minimal task management web app with light/dark mode toggle, task filtering, and local storage.",
+      "techStack": ["HTML", "CSS","JavaScript"],
+      "image": taskwaveImg,
+      "demoLink": "https://dagi-taskwave.netlify.app/",
+      "codeLink": "https://github.com/DagiH22/Task-Wave"
     },
     {
       "id": 2,
-      "title": "TaskWave",
-      "description": "A minimal task management web app with light/dark mode toggle, task filtering, and local storage.",
-      "techStack": ["React", "Tailwind CSS"],
-      "image": img,
-      "demoLink": "https://your-demo-link.com/taskwave",
-      "codeLink": "https://github.com/yourusername/taskwave"
-    },
-    {
-      "id": 3,
-      "title": "DevBlog",
-      "description": "A markdown-based blog platform with a Node.js + Express backend and MongoDB database.",
-      "techStack": ["Node.js", "Express", "MongoDB", "EJS"],
-      "image": img,
-      "demoLink": "https://your-demo-link.com/devblog",
-      "codeLink": "https://github.com/yourusername/devblog"
-    },
-    {
-      "id": 4,
-      "title": "Portify",
-      "description": "This very portfolio — a sleek and minimal site built with React and CSS modules to showcase my work and interests.",
-      "techStack": ["React", "CSS Modules"],
-      "image": img,
-      "demoLink": "https://your-portfolio.com",
+      "title": "Portfolio",
+      "description": "This very portfolio — a sleek and minimal site built with React and CSS to showcase my work and interests.",
+      "techStack": ["React", "CSS"],
+      "image": portfolio,
+      "demoLink": "https://dagi-portfolio.netlify.app/",
       "codeLink": "https://github.com/yourusername/portfolio"
     }
   ]
