b9cf42029575eda9010f3579ccafdcab4f7916c3
Dagi <dagiheywot@gmail.com>
2025-08-15 18:07:17 +0300

    update resume

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useEffect, useState } from "react";
 import '../styles/Home.css'
-import resume from "../../assets/cv/Dagmawi-Heywot-CV.pdf"
+import resume from "../../assets/cv/Dagmawi-Heywot-Resume.pdf"
 
 function Home() {
   const text = "Always learning. Always building.";
